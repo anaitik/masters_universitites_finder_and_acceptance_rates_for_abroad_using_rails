ba2d1a609180411762c5a4abfb8962fa0298e7ec
anaitik <nitikverma72@gmail.com>
2023-08-26 00:37:24 +0530

    adding event active admin view

---
+++ app/admin/bx_block_event_events.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register BxBlockEvent::Event, as: "Event"do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :link, :name, :event_date, :event_time, :description, :image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:link, :name, :event_date, :event_time, :description, :image]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
