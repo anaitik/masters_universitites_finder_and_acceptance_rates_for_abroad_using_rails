00df5fef7ddf14ae7c5948541b5132c8cbd4ffc3
Project <projectp907@gmail.com>
2023-09-09 20:28:42 +0530

    added sub sub sub courses

--- app/models/bx_block_course/sub_sub_course.rb
+++ app/models/bx_block_course/sub_sub_course.rb
@@ -1,5 +1,6 @@
-module BxBlockCourses
+module BxBlockCourse
   class SubSubCourse < ApplicationRecord
-    belongs_to :sub_course, class_name: 'BxBlockCourses::SubCourse'
+    belongs_to :sub_course, class_name: 'BxBlockCourse::SubCourse'
+    has_many :sub_sub_sub_courses, class_name: 'BxBlockCourse::SubSubSubCourse'
   end
 end
